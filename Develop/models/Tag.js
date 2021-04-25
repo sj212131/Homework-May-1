1c1a6b4a772a6e6ddfa0b40e3a2c28bbe14a907b
shangxian Piao <sj212131@hotmail.com>
2021-04-25 17:59:23 -0400

    redo homework and uplode the offered file

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,20 +1,12 @@
 const { Model, DataTypes } = require('sequelize');
+
 const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
- 
-    tag_name: {
-      type: DataTypes.STRING
-    }
+    // define columns
   },
   {
     sequelize,
